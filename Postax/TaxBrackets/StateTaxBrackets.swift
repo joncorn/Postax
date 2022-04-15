6cd57333360aae0757c99ab4bd00b29b6c19293e
Jon Corn <joncorn@Jons-MacBook-Pro.local>
2022-04-14 21:59:26 -0700

    outline state tax model

--- Postax/TaxBrackets/StateTaxBrackets.swift
+++ Postax/TaxBrackets/StateTaxBrackets.swift
@@ -10,41 +10,254 @@ import Foundation
 // State tax brackets, sorted alphabetically
 struct StateTaxBrackets {
     
+    // MARK: - Alabama
     struct Alabama {
         
     }
     
+    // MARK: - Alaska
     struct Alaska {
         
     }
     
+    // MARK: - Arizona
     struct Arizona {
         
     }
     
+    // MARK: - Arkansas
     struct Arkansas {
         
     }
     
+    // MARK: - California
     struct California {
         
     }
     
+    // MARK: - Colorado
     struct Colorado {
         
     }
     
+    // MARK: - Connecticut
     struct Connecticut {
         
     }
     
+    // MARK: - Delaware
     struct Delaware {
         
     }
     
+    // MARK: - Florida
     struct Florida {
         
     }
     
+    // MARK: - Georgia
+    struct Georgia {
+        
+    }
+    
+    // MARK: - Hawaii
+    struct Hawaii {
+        
+    }
+    
+    // MARK: - Idaho
+    struct Idaho {
+        
+    }
+    
+    // MARK: - Illinois
+    struct Illinois {
+        
+    }
+    
+    // MARK: - Indiana
+    struct Indiana {
+        
+    }
+    
+    // MARK: - Iowa
+    struct Iowa {
+        
+    }
+    
+    // MARK: - Kansas
+    struct Kansas {
+        
+    }
+    
+    // MARK: - Kentucky
+    struct Kentucky {
+        
+    }
+    
+    // MARK: - Louisiana
+    struct Louisiana {
+        
+    }
+    
+    // MARK: - Maine
+    struct Maine {
+        
+    }
+    
+    // MARK: - Maryland
+    struct Maryland {
+        
+    }
+    
+    // MARK: - Massachusetts
+    struct Massachusetts {
+        
+    }
+    
+    // MARK: - Michigan
+    struct Michigan {
+        
+    }
+    
+    // MARK: - Minnesota
+    struct Minnesota {
+        
+    }
+    
+    // MARK: - Mississippi
+    struct Mississippi {
+        
+    }
+    
+    // MARK: - Missouri
+    struct Missouri {
+        
+    }
+    
+    // MARK: - Montana
+    struct Montana {
+        
+    }
+    
+    // MARK: - Nebraska
+    struct Nebraska {
+        
+    }
+    
+    // MARK: - Nevada
+    struct Nevada {
+        
+    }
+    
+    // MARK: - New Hampshire
+    struct NewHampshire {
+        
+    }
+    
+    // MARK: - New Jersey
+    struct NewJersey {
+        
+    }
+    
+    // MARK: - New Mexico
+    struct NewMexico {
+        
+    }
+    
+    // MARK: - New York
+    struct NewYork {
+        
+    }
+    
+    // MARK: - North Carolina
+    struct NorthCarolina {
+        
+    }
+    
+    // MARK: - North Dakota
+    struct NorthDakota {
+        
+    }
+    
+    // MARK: - Ohio
+    struct Ohio {
+        
+    }
+    
+    // MARK: - Oklahoma
+    struct Oklahoma {
+        
+    }
+    
+    // MARK: - Oregon
+    struct Oregon {
+        
+    }
+    
+    // MARK: - Pennsylvania
+    struct Pennsylvania {
+        
+    }
+    
+    // MARK: - Rhode Island
+    struct RhodeIsland {
+        
+    }
+
+    // MARK: - South Carolina
+    struct SouthCarolina {
+        
+    }
+    
+    // MARK: - South Dakota
+    struct SouthDakota {
+        
+    }
+    
+    // MARK: - Tennessee
+    struct Tennessee {
+        
+    }
+    
+    // MARK: - Texas
+    struct Texas {
+        
+    }
+    
+    // MARK: - Utah
+    struct Utah {
+        
+    }
+    
+    // MARK: - Vermont
+    struct Vermont {
+        
+    }
+    
+    // MARK: - Virginia
+    struct Virginia {
+        
+    }
+    
+    // MARK: - Washington
+    struct Washington {
+        
+    }
+    
+    // MARK: - West Virginia
+    struct WestVirginia {
+        
+    }
+    
+    // MARK: - Wisconsin
+    struct Wisconsin {
+        
+    }
+    
+    // MARK: - Wyoming
+    struct Wyoming {
+        
+    }
     
 }
