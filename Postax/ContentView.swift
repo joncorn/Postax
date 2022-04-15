6996db1d810283a6c1d4c2bea95b54d11b9be72b
Jon Corn <joncorn@Jons-MacBook-Pro.local>
2022-04-15 01:56:07 -0700

    fixed fed tax bracket logic

--- Postax/ContentView.swift
+++ Postax/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var annual = 41600.0
+    
+    @State private var taxes = 0.0
+    @State private var fedTaxes = 0.0
+    @State private var stateTaxes = 0.0
+    @State private var net = 0.0
+    @State private var paycheck = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Taxes: \(paycheck)")
+                .padding()
+            Button("Calculate") {
+                fedTaxes = FedTaxBrackets.FedTaxAmount(from: annual)
+                net = annual - fedTaxes
+                paycheck = net / 26
+            }
+        }
     }
 }
 
--- Postax/TaxBrackets/FedTaxBrackets.swift
+++ Postax/TaxBrackets/FedTaxBrackets.swift
@@ -10,6 +10,10 @@ import Foundation
 // Federal tax brackets, percent and threshold
 struct FedTaxBrackets {
     
+    // standard tax percent across all states
+    static let SSPercent = 0.062
+    static let medicarePercent = 0.0145
+    
     struct first {
         static let percent   = 0.10
         static let threshold = 9950.0
@@ -40,18 +44,55 @@ struct FedTaxBrackets {
     }
     
     // returns federal tax amount as Double
-    func FedTaxAmount(from annual: Double) -> Double {
+    static func FedTaxAmount(from annual: Double) -> Double {
         
-        var fedTax: Double = 0
+        let SSTax = annual * SSPercent
+        let medicareTax = annual * medicarePercent
+        
+        var firstTaxed   : Double = 0
+        var secondTaxed  : Double = 0
+        var thirdTaxed   : Double = 0
+        var fourthTaxed  : Double = 0
+        var fifthTaxed   : Double = 0
+        var sixthTaxed   : Double = 0
+        var seventhTaxed : Double = 0
+        
+        var fedTax = SSTax + medicareTax
         
         if annual <= first.threshold {
             fedTax = annual * first.percent
         } else if annual <= second.threshold {
-            fedTax = annual * second.percent
+            
+            // first bracket, times it's tax percent
+            firstTaxed = first.threshold * first.percent
+            // annual minus first tax bracket, so you're left with what's in second tax bracket
+            secondTaxed = annual - first.threshold
+            secondTaxed = secondTaxed * second.percent
+            
+            fedTax += firstTaxed + secondTaxed
+            
         } else if annual <= third.threshold {
-            fedTax = annual * third.percent
+            
+            firstTaxed = first.threshold * first.percent
+            secondTaxed = second.threshold - first.threshold
+            secondTaxed = secondTaxed * second.percent
+            
+            thirdTaxed = annual - second.threshold
+            thirdTaxed = thirdTaxed * third.percent
+            
+            fedTax += firstTaxed + secondTaxed + thirdTaxed
+            
         } else if annual <= fourth.threshold {
-            fedTax = annual * fourth.percent
+            
+            firstTaxed = first.threshold * first.percent
+            secondTaxed = second.threshold * second.percent
+            thirdTaxed = third.threshold * third.percent
+            
+            fourthTaxed = annual - third.threshold
+            fourthTaxed = fourthTaxed * fourth.percent
+            
+            fedTax += firstTaxed + secondTaxed + thirdTaxed + fourthTaxed
+            
         } else if annual <= fifth.percent {
             fedTax = annual * fifth.threshold
         } else if annual <= sixth.percent {
@@ -62,5 +103,4 @@ struct FedTaxBrackets {
         
         return fedTax
     }
-    
 }
